61f651f42b7e703ceb77224199195cf71bf5db92
wrewsama <andrewlzs512@gmail.com>
2022-06-13 15:31:02 +0800

    Set up backend script to work with simplet5 model

--- backend/backend.py
+++ backend/backend.py
@@ -1,13 +1,17 @@
 import time
 from flask import Flask, request, jsonify
-from transformers import pipeline
+from simplet5 import SimpleT5
 
 app = Flask(__name__)
-summariser = pipeline("summarization")
+# Create model
+model = SimpleT5() 
+# Load model parameters
+model.load_model("t5", "backend/simplet5model", use_gpu=True)
 
 @app.route('/summarize', methods=['POST'])
 def add():
     data = request.get_json()
-    summary = summariser(data, max_length=100, min_length=50, do_sample=False)[0]['summary_text']
+    # Get summary
+    summary = model.predict("summarize: " + data)
 
     return jsonify({ 'summary': summary })
