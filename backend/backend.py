b231e3d2cd786245a3a44b57cd1c8d668c0a6355
wrewsama <andrewlzs512@gmail.com>
2022-06-14 15:15:18 +0800

    change codebase to use extractive text summarisation with spaCy

--- backend/backend.py
+++ backend/backend.py
@@ -1,17 +1,54 @@
-import time
 from flask import Flask, request, jsonify
-from simplet5 import SimpleT5
+import spacy
+from spacy.lang.en.stop_words import STOP_WORDS
+import string
+from heapq import nlargest
 
 app = Flask(__name__)
-# Create model
-model = SimpleT5() 
-# Load model parameters
-model.load_model("t5", "../outputs/simplet5-epoch-2-train-loss-0.8114-val-loss-1.4894", use_gpu=True)
+
+"""i have no idea what this does i just copypasted it"""
+stopwords = list(STOP_WORDS)
+
+nlp = spacy.load('en_core_web_sm')
 
 @app.route('/summarize', methods=['POST'])
 def add():
     data = request.get_json()
     # Get summary
-    summary = model.predict("summarize: " + data)
+    doc = nlp(data)
 
+    punctuation = string.punctuation + '\n'
+
+    word_frequencies = {}
+    for word in doc:
+        if word.text.lower() not in stopwords:
+            if word.text.lower() not in punctuation:
+                if word.text not in word_frequencies.keys():
+                    word_frequencies[word.text] = 1
+                else:
+                    word_frequencies[word.text] += 1
+                    
+
+    max_frequency = max(word_frequencies.values())
+
+    for word in word_frequencies.keys():
+        word_frequencies[word] = word_frequencies[word]/max_frequency
+
+    sentence_tokens = [sent for sent in doc.sents]
+
+    sentence_scores = {}
+    for sent in sentence_tokens:
+        for word in sent:
+            if word.text.lower() in word_frequencies.keys():
+                if sent not in sentence_scores.keys():
+                    sentence_scores[sent] = word_frequencies[word.text.lower()]
+                else:
+                    sentence_scores[sent] += word_frequencies[word.text.lower()]
+                    
+    select_length = int(len(sentence_tokens)*0.3)
+
+    summary = nlargest(select_length, sentence_scores, key = sentence_scores.get)
+
+    final_summary = [word.text for word in summary]
+    summary = ' '.join(final_summary)
     return jsonify({ 'summary': summary })
