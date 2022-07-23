afe0623de40262a6f33c453ddbfb4bc8b5937512
wrewsama <andrewlzs512@gmail.com>
2022-07-23 23:54:08 +0800

    Add functions to facilitate the changing of the summary length

--- backend/backend.py
+++ backend/backend.py
@@ -7,12 +7,24 @@ app = Flask(__name__)
 # Initialise a summary pipeline with the news model as the default
 summariser = pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")
 
+# Initialise global variable to store the word limit
+word_limit = 100
+
 @app.route('/summarize', methods=['POST'])
 def add():
     data = request.get_json()
     summary = summariser(data, max_length=word_limit, min_length=30, do_sample=False)[0]['summary_text']
 
     return jsonify({ 'summary': summary })
+
+def change_word_limit(new_limit: int) -> None:
+    """Changes the word limit to the specified number"""
+
+    # Refer to the global word limit variable
+    global word_limit
+
+    # Change the word limit to the input
+    word_limit = new_limit
 
 def switch_model_to_news() -> None:
     """Switches the summariser to news mode."""
