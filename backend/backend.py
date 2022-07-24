a3f93187546721b45a004aa0cbf18ffae91e36f2
Rama <kmram0908@gmail.com>
2022-07-24 11:50:03 +0800

    integrated frontend with different model selection and wordlimit

--- backend/backend.py
+++ backend/backend.py
@@ -4,50 +4,22 @@ from transformers import pipeline
 
 app = Flask(__name__)
 
-# Initialise a summary pipeline with the news model as the default
-summariser = pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")
-
-# Initialise global variable to store the word limit
-word_limit = 100
+models = {
+    'default': None,
+    'news': "sshleifer/distilbart-cnn-12-6",
+    'paper': "google/pegasus-pubmed",
+    'book': "pszemraj/long-t5-tglobal-base-16384-book-summary"
+}
 
 @app.route('/summarize', methods=['POST'])
 def add():
     data = request.get_json()
-    summary = summariser(data, max_length=word_limit, min_length=30, do_sample=False)[0]['summary_text']
+    content = data['content']
+    wordLimit = data['wordLimit']
+    summaryMode = data['summaryMode']
+    summariser = pipeline("summarization", model=models[summaryMode])
+    summary = summariser(content, max_length=wordLimit, min_length=30, do_sample=False)[0]['summary_text']
 
     return jsonify({ 'summary': summary })
 
-def change_word_limit(new_limit: int) -> None:
-    """Changes the word limit to the specified number"""
-
-    # Refer to the global word limit variable
-    global word_limit
-
-    # Change the word limit to the input
-    word_limit = new_limit
-
-def switch_model_to_news() -> None:
-    """Switches the summariser to news mode."""
-
-    switch_model_to("sshleifer/distilbart-cnn-12-6")
-
-def switch_model_to_paper() -> None:
-    """Switches the summariser to scientific paper mode."""
-
-    switch_model_to("google/pegasus-pubmed")
-
-def switch_model_to_book() -> None:
-    """Switches the summariser to book mode."""
-
-    switch_model_to("pszemraj/long-t5-tglobal-base-16384-book-summary")
-
-def switch_model_to(modelname: str) -> None:
-    """Changes the summariser to a specified model."""
-    
-    # Refer to the global variable named "summariser"
-    # which holds our summariser pipeline
-    global summariser
-
-    # Changes it to the news model
-    summariser = pipeline("summarization", model=modelname)
 
--- src/pages/Summarizer.jsx
+++ src/pages/Summarizer.jsx
@@ -6,19 +6,33 @@ import LinearProgress from '@mui/material/LinearProgress';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogTitle from '@mui/material/DialogTitle';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
 import { authMiddleware } from '../util/auth';
 
+const modesOfSummary = ['default', 'news', 'paper', 'book'];
+
 function Summarizer() {
   const navigate = useNavigate();
 
+  const [wordLimit, setWordLimit] = useState(100);
+  const [summaryMode, setSummaryMode] = useState(modesOfSummary[0]);
   const [content, setContent] = useState('');
   const [sumarized, setSumarized] = useState('');
   const [title, setTitle] = useState('');
   const [titlePrompt, setTitlePrompt] = useState(false);
   const [loading, setLoading] = useState(false);
+
+  const handleWordLimit = (event) => {
+    setWordLimit(event.target.value);
+  }
+
+  const handleSummaryMode = (event) => {
+    setSummaryMode(event.target.value);
+  }
 
   const handleTitleClose = () => {
     setTitle('');
@@ -44,7 +58,7 @@ function Summarizer() {
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify(content)
+      body: JSON.stringify({ content, wordLimit, summaryMode })
     })
       .then(res => res.json())
       .then(obj => {
@@ -88,7 +102,7 @@ function Summarizer() {
       sx={{
         width: '100%',
         height: '100vh',
-        backgroundColor: '#D7EAE9',
+        // backgroundColor: '#D7EAE9',
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'center',
@@ -96,6 +110,18 @@ function Summarizer() {
         gap: '20px'
       }}
     >
+      <Box display="flex" width="70%" gap="24px" alignItems="center" justifyContent="space-between">
+        <TextField value={wordLimit} variant='standard' sx={{ width: '72px' }} label="word limit" onChange={handleWordLimit}/>
+        <Select value={summaryMode} onChange={handleSummaryMode} sx={{ width: '196px' }}>
+          {
+            modesOfSummary.map((mode) => (
+              <MenuItem value={mode}>
+                {mode}
+              </MenuItem>
+            ))
+          }
+        </Select>
+      </Box>
       <TextField
         value={content}
         onChange={handleTextInput}
